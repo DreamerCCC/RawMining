36b6098597c596b87c5ae40936d1e5e5fd356c4b
DreamerCCC <787818219@qq.com>
2023-12-12 12:34:34 +0800

    update

---
+++ configs/funcfr.py
@@ -0,0 +1,14 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+import torch.nn as nn
+
+from .registry import ACTIVATION_LAYERS
+
+@ACTIVATION_LAYERS.register_module()
+class MyFR(nn.Module):
+    def __init__(self, inplace: bool = False):
+        super().__init__()
+        self.act = nn.ReLU6(inplace)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.clamp((x * self.act(x + 3) / 6), max=8)
